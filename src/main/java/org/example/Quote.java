39dd084f3f4a1a7f13f0787b9ae91a94699197d3
Daniela <danigomezdugan@gmail.com>
2021-09-07 16:48:48 -0400

    updating file

--- src/main/java/org/example/Quote.java
+++ src/main/java/org/example/Quote.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 /**
  * UCF COP3330 Fall 2021 Assignment 1 Solution
- *Copyright 2021 daniela gomez-dugan
+ *Copyright 2021 Daniela Gomez-Dugan
  */
 public class Quote
 {
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("What is the quote?");
+        System.out.print("What is the quote? ");
         String quote = input.nextLine();
-        System.out.println("Who said it?");
+        System.out.print("Who said it? ");
         String person = input.nextLine();
         System.out.println(person + " says, \"" + quote + "\"");
     }
